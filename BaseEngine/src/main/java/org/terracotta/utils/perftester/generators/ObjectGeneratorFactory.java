78dc1232406dd9bcfa5a23fdf46535717b097fdb
Fabien Sanglier <fsanglie@terracottatech.com>
2012-12-19 16:02:41 -0500

    refactoring the folders a bit...

---
+++ BaseEngine/src/main/java/org/terracotta/utils/perftester/generators/ObjectGeneratorFactory.java
@@ -0,0 +1,9 @@
+package org.terracotta.utils.perftester.generators;
+
+/**
+ * @author Fabien Sanglier
+ * 
+ */
+public interface ObjectGeneratorFactory {
+	ObjectGenerator createObjectGenerator();
+}
